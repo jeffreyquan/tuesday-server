6cb7267ebf894d63b35c2571b48d16028767cb0f
Jeffrey Quan <jeffrey.quan@hotmail.com>
2019-11-23 00:41:57 +1100

    created project model

---
+++ db/migrate/20191122134032_create_projects.rb
@@ -0,0 +1,10 @@
+class CreateProjects < ActiveRecord::Migration[6.0]
+  def change
+    create_table :projects do |t|
+      t.text :name
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
