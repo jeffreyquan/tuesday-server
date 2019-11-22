2fc64ec0ed5c9cca7cd25a5899c143bb28459e8e
Jeffrey Quan <jeffrey.quan@hotmail.com>
2019-11-23 09:06:31 +1100

    updated membership controller

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -1,3 +1,14 @@
 class MembershipsController < ApplicationController
 
+
+
+  def index
+    if @current_user.present? && @current_user.admin === true
+      @memberships = Membership.all
+    else
+      @memberships = Membership.where(:user_id => @current_user.id)
+    end
+
+    render json: @memberships
+  end
 end
