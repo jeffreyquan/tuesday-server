1ca2a615b63389d78a8fdbb50b660d6fa7a5da67
Jeffrey Quan <jeffrey.quan@hotmail.com>
2019-11-23 09:07:04 +1100

    setting up tasks controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,38 @@
 class TasksController < ApplicationController
+
+
+  # TODO: limit tasks to only those belonging to 
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def edit
+
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:name, :status, :due_date, :priorty, :owner, :project_id)
+  end
+
 end
