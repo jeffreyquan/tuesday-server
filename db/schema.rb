05b762307978fb2ba125e815c66078581ae9a6db
Jeffrey Quan <jeffrey.quan@hotmail.com>
2019-11-23 02:09:00 +1100

    ran db migrate -- updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,38 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_22_081338) do
+ActiveRecord::Schema.define(version: 2019_11_22_145538) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "memberships", force: :cascade do |t|
+    t.integer "project_id"
+    t.string "user_id"
+    t.boolean "admin"
+    t.boolean "invitation"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "projects", force: :cascade do |t|
+    t.text "name"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.integer "project_id"
+    t.text "name"
+    t.text "status"
+    t.datetime "due_date"
+    t.text "priority"
+    t.text "owner"
+    t.text "group"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.text "email"
