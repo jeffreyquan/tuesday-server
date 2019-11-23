2e22d816b5cb5228dd38ef55777b1b1bf210cbf2
Jeffrey Quan <jeffrey.quan@hotmail.com>
2019-11-23 23:01:28 +1100

    updated cors file with new origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,7 +9,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end
 
   allow do
-    origins 'https://client-tuesday.now.sh/'
+    origins 'https://client-tuesday.markfranco.now.sh/'
     resource '*',
       :headers => :any,
       :methods => %i( get post put patch delete options head ),
