27222795bbf8ca1a72d63a285306e193753d1235
Jeffrey Quan <jeffrey.quan@hotmail.com>
2019-11-26 10:32:09 +1100

    i didn't change anything?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,4 +14,9 @@ class ApplicationController < ActionController::Base
     puts decoded_token
   end
 
+  private
+  def fetch_user
+    @current_user = User.find_by :id => session[:user_id] if session[:user_id].present?
+    session[:user_id] = nil unless @current_user.present?
+  end
 end
