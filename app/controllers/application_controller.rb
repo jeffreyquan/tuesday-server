078c09fecca6d3bd6d83cee7367baf7aa9e8b710
Jeffrey Quan <jeffrey.quan@hotmail.com>
2019-11-26 17:32:32 +1100

    removed puts around authenticate function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token
   include ::ActionController::Cookies
-  before_action :fetch_user
 
   def bearer_token
     pattern = /^Bearer /
@@ -10,16 +9,6 @@ class ApplicationController < ActionController::Base
   end
 
   def authenticate_user
-    puts bearer_token
     decoded_token = JWT.decode(bearer_token, "mys3cr3t", true, {algorithm: 'HS256'})
-    puts decoded_token
-  end
-
-  def fetch_user
-    puts "fetching user"
-    puts session[:user_id]
-    @current_user = User.find_by :id => session[:user_id] if session[:user_id].present?
-    session[:user_id] = nil unless @current_user.present?
-    puts @current_user
   end
 end
