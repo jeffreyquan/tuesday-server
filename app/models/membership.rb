c99192fbc353f462154769842dfed87436ee170e
Jeffrey Quan <jeffrey.quan@hotmail.com>
2019-11-27 11:33:40 +1100

    validate uniqueness of email and project id in memberships

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -2,4 +2,6 @@ class Membership < ApplicationRecord
   belongs_to :user, :optional => true
   belongs_to :project, :optional => true
   has_many :tasks, :through => :projects
+
+  validates_uniqueness_of :email, :scope => :project_id 
 end
