f2b4579e9e2e1570337669e29a517c6bccf694e6
Jeffrey Quan <jeffrey.quan@hotmail.com>
2019-11-28 21:45:27 +1100

    more email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
 
-  has_many :memberships
+  has_many :memberships, dependent: :destroy
   has_many :projects, :through => :memberships
 
-  validates :email, presence: true
-  validates_uniqueness_of :email
+  validates :email, :presence => true, :uniqueness => { :case_sensitive => false }, :length => { :minimum => 5 }
 end
