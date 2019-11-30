737f8939421e760ecfdee77409fdfff93bde9f6b
Jeffrey Quan <jeffrey.quan@hotmail.com>
2019-12-01 00:54:41 +1100

    added gh page link to cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -18,4 +18,13 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       :credentials => true
   end
 
+    allow do
+      origins 'https://jeffreyquan.github.io/tuesday-client/'
+      resource '*',
+        :headers => :any,
+        :expose => ["jwt"],
+        :methods => %i( get post put patch delete options head ),
+        :credentials => true
+    end
+
 end
