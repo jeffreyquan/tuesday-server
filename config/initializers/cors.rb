8676ee3f4be3bab23fccb68cdc36749a45b9458d
Jeffrey Quan <jeffrey.quan@hotmail.com>
2019-12-01 22:54:32 +1100

    updated cors to test gh pages

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -19,7 +19,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end
 
   allow do
-    origins 'https://jeffreyquan.github.io'
+    origins 'https://jeffreyquan.github.io/tuesday-client/#/'
     resource '*',
       :headers => :any,
       :expose => ["jwt"],
